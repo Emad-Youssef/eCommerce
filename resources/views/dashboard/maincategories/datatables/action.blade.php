7d5bf0099788cec0f7d3ff68ea0e64c11b0e72d9
يوسف الفولى <emad.elfoly99@gmail.com>
2020-12-11 00:10:30 +0200

    =fix bug id on delete & edit  mainCategory

--- resources/views/dashboard/maincategories/datatables/action.blade.php
+++ resources/views/dashboard/maincategories/datatables/action.blade.php
@@ -6,9 +6,9 @@
     </button>
     <div class="dropdown-menu" role="menu" x-placement="bottom-start"
         style="position: absolute; transform: translate3d(0px, 38px, 0px); top: 0px; right: -38px; will-change: transform;">
-        <a href="{{route('admin.mainCategory.edit',$id)}}" class="dropdown-item btn btn-outline-primary"><i
+        <a href="{{route('admin.mainCategory.edit',$category_id)}}" class="dropdown-item btn btn-outline-primary"><i
                 class="la la-edit"></i> @lang('site.edit')</a>
-        <a data-route="{{ route('admin.mainCategory.destroy',$id) }}"
+        <a data-route="{{ route('admin.mainCategory.destroy',$category_id) }}"
             data-token="{{ csrf_token() }}"
             data-msdelete="{{ __('messages.msdelete') }}"
             data-yes="{{ __('site.yes') }}"
